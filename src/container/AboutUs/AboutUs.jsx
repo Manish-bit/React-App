c552898910b6af3b141ecd762b4329798b52956a
Manish-Bit <manisharyal517@gmail.com>
2022-06-17 20:50:17 +0545

    Feature: about and special menu added

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
-
+import { images } from '../../constants';
 import './AboutUs.css';
 
-const AboutUs = () => (
-  <div>
-    AboutUs
-  </div>
-);
+const AboutUs = () => {
+    return(
+      <>
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+    <div className='app__about-overlay flex__center'>
+      <img src={images.G} alt='g letter'/>
+    </div>
+     <div className='app__aboutus-content flex__center'>
+      <div className='app__aboutus-content_about'>
+       <h1 className='headtext__cormorant'>About  Us</h1>
+       <img src={images.spoon} alt="about_spoon" className='spoon__img'/>
+     <p className='p__opensans'>hweprasldkfjba asdfhafakjdfbswuefiwuf</p>
+     <button type='button' className='custom__button'>Know more</button>
+      </div>
+
+       <div className='app__aboutus-content_knife flex__center'>
+        <img src={images.knife} alt="about_knife"/>
+      </div>
+
+      <div className='app__aboutus-content_history'>
+       <h1 className='headtext__cormorant'>Our history</h1>
+       <img src={images.spoon} alt="about_spoon" className='spoon__img'/>
+     <p className='p__opensans'>hweprasldkfjba asdfhafakjdfbswuefiwuf</p>
+     <button type='button' className='custom__button'> Know more</button>
+      </div>
+      </div>
+    </div>  
+  </>
+    )
+};
 
 export default AboutUs;
